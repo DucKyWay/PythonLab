bf1d9c2f6eecee3c0bf086f0c438f7ca728bdf1b
DucKyWay <niji2549@gmail.com>
2024-06-20 15:52:07 +0700

    add 101-1 start and sort function

--- elab_helloworld/101/1_uno.py
+++ elab_helloworld/101/1_uno.py
@@ -2,7 +2,27 @@
 #------------------ Hello to python 101 elab yay ------------------#
 #------------------------------------------------------------------#
 # https://elab.punsnx.com/elab/lab/1/22/
+
+player_cards = [[], [], [], []]
+sorted_cards = [[], [], [], []]
+number_priority = {str(i): i for i in range(10)}
+color_priority = {'R': 0, 'G': 1, 'B': 2, 'Y': 3} # --> red green blue yellow
+
+def sorted_card(card):
+    num, color = card[0], card[1]
+    return (color_priority[color], number_priority[num])
+
+def start():
+    for i in range(4):
+        card = input()
+        player_cards[i] = list(card.split(" "))
+        sorted_cards[i] = sorted(player_cards[i], key=sorted_card)
+    start_game = input()
+
 def main():
-    print()
+    print(start())
+
+    print(player_cards)
+    print(sorted_cards)
 
 main()
