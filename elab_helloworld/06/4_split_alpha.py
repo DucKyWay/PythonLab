1bc508c943fe448ee8dd80d51b924c1040d396cb
DucKyWay <niji2549@gmail.com>
2024-06-20 14:07:26 +0700

    add 06-04 final and add file 101

--- elab_helloworld/06/4_split_alpha.py
+++ elab_helloworld/06/4_split_alpha.py
@@ -4,5 +4,44 @@
 
 def main():
     txt = input("Please input string: ")
+    alphabet = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z', 
+                'B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z']
+    number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    vowel = ["a", "e", "i", "o", "u", 
+            "A", "E", "I", "O", "U"]
+    split_txt = list(txt)
+    chk_alp, chk_num, chk_vol = [], [], []
+
+    for i in split_txt:
+        if i in alphabet:
+            chk_alp.append(i)
+        elif i in number:
+            chk_num.append(i)
+        elif i in vowel:
+            chk_vol.append(i)
+
+    if chk_vol != []:
+        print("Vowel(s):")
+        for i in range(len(chk_vol)):
+            if i != len(chk_vol)-1:
+                print(chk_vol[i], end=" ")
+            else:
+                print(chk_vol[i])
+                
+    if chk_alp != []:
+        print("Alphabet(s):")
+        for i in range(len(chk_alp)):
+            if i != len(chk_alp)-1:
+                print(chk_alp[i], end=" ")
+            else:
+                print(chk_alp[i])
+
+    if chk_num != []:
+        print("Number(s):")
+        for i in range(len(chk_num)):
+            if i != len(chk_num)-1:
+                print(chk_num[i], end=" ")
+            else:
+                print(chk_num[i])
 
 main()
---
+++ elab_helloworld/06/5_camel_case.py
@@ -0,0 +1,12 @@
+#------------------------------------------------------------------#
+#------------------ Hello to python 101 elab yay ------------------#
+#------------------------------------------------------------------#
+
+# -I?LOVE-pythON?PROgRaMMIng?
+# iLovePythonProgramming
+# https://elab.punsnx.com/elab/lab/1/19/38/
+
+def main():
+    print()
+
+main()
