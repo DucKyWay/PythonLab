ec82c24e1f922052d791377c92625264c46b10fa
DucKyWay <niji2549@gmail.com>
2024-06-20 16:41:01 +0700

    add gameplay uno

--- elab_helloworld/101/1_uno.py
+++ elab_helloworld/101/1_uno.py
@@ -6,7 +6,8 @@
 player_cards = [[], [], [], []]
 sorted_cards = [[], [], [], []]
 number_priority = {str(i): i for i in range(10)}
-color_priority = {'R': 0, 'G': 1, 'B': 2, 'Y': 3} # --> red green blue yellow
+color_priority = {'R': 0, 'G': 1, 'B': 2, 'Y': 3} # --> r g b y
+start_game, turn = "", ""
 
 def sorted_card(card):
     num, color = card[0], card[1]
@@ -18,9 +19,19 @@ def start():
         player_cards[i] = list(card.split(" "))
         sorted_cards[i] = sorted(player_cards[i], key=sorted_card)
     start_game = input()
+    return start_game
+
+def gameplay(start_game):
+    while True:
+        for i in range(4):
+            print(i)
+            print(f"Player {i+1} place turn card to match 6R in both number and color")
+        break
+    
 
 def main():
-    print(start())
+    start()
+    gameplay(start_game)
 
     print(player_cards)
     print(sorted_cards)
